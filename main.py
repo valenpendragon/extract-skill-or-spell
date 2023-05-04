78f25c4025da4dea38aedae9b28981f3013ed37d
Jeffrey Scott <valenpendragon@gmail.com>
2023-05-04 12:23:48 -0400

    Move main window creation to function make_main_window and event loop to function main.

--- main.py
+++ main.py
@@ -12,58 +12,77 @@ EXTRAS = ["Improvised Weapons", "Punch", "Dirty Fighting", "Kick",
 
 sg.theme("Black")
 
-# Here is the main window.
-file_choice_label = sg.Text("Select the file to extract spell, skills or talents from:")
-file_choice_input = sg.Input(key="file_choice",
-                             enable_events=True,
-                             visible=True)
-file_choice_button = sg.FileBrowse("Choose",
-                                   key="filepath",
-                                   target="file_choice")
-layout_col1 = [[file_choice_label],
-               [file_choice_input],
-               [file_choice_button]]
 
-output_dir_label = sg.Text("Select the destination directory:")
-output_dir_input = sg.Input(key="folder_choice",
-                            enable_events=True,
-                            visible=True)
-output_dir_button = sg.FolderBrowse("Choose",
-                                    key="dest_folder",
-                                    target="folder_choice")
-filename_label = sg.Text("Enter the name of the output file:")
-filename_tooltip = "Avoid overwriting the original file if possible."
-filename_input = sg.Input(key="filename",
-                          enable_events=True,
-                          visible=True,
-                          tooltip=filename_tooltip)
-layout_col2 = [[output_dir_label],
-               [output_dir_input],
-               [output_dir_button],
-               [filename_label],
-               [filename_input]]
+def make_main_window() -> sg.Window:
+    """
+    This function creates and returns a main_window.
+    :return: sg.Window
+    """
+    # Here is the main window.
+    file_choice_label = sg.Text("Select the file to extract spell, skills or talents from:")
+    file_choice_input = sg.Input(key="file_choice",
+                                 enable_events=True,
+                                 visible=True)
+    file_choice_button = sg.FileBrowse("Choose",
+                                       key="filepath",
+                                       target="file_choice")
+    layout_col1 = [[file_choice_label],
+                   [file_choice_input],
+                   [file_choice_button]]
 
-col1 = sg.Column(layout=layout_col1)
-col2 = sg.Column(layout=layout_col2)
+    output_dir_label = sg.Text("Select the destination directory:")
+    output_dir_input = sg.Input(key="folder_choice",
+                                enable_events=True,
+                                visible=True)
+    output_dir_button = sg.FolderBrowse("Choose",
+                                        key="dest_folder",
+                                        target="folder_choice")
+    filename_label = sg.Text("Enter the name of the output file:")
+    filename_tooltip = "Avoid overwriting the original file if possible."
+    filename_input = sg.Input(key="filename",
+                              enable_events=True,
+                              visible=True,
+                              tooltip=filename_tooltip)
+    layout_col2 = [[output_dir_label],
+                   [output_dir_input],
+                   [output_dir_button],
+                   [filename_label],
+                   [filename_input]]
 
-convert_file_button = sg.Button("Convert File", key="convert")
-exit_button = sg.Button("Exit", key="exit")
-result_label = sg.Text(key="results", visible=False, text_color="white")
-bottom_row_layout = [[convert_file_button, exit_button, result_label]]
+    col1 = sg.Column(layout=layout_col1)
+    col2 = sg.Column(layout=layout_col2)
 
-main_window = sg.Window("Spell, Talent, and Skill Extractor",
-                        layout=[[col1],
-                                [col2],
-                                [bottom_row_layout]])
+    convert_file_button = sg.Button("Convert File", key="convert")
+    exit_button = sg.Button("Exit", key="exit")
+    result_label = sg.Text(key="results", visible=False, text_color="white")
+    bottom_row_layout = [[convert_file_button, exit_button, result_label]]
 
-while True:
-    event, values = main_window.read()
-    print(event, values)
+    return sg.Window("Spell, Talent, and Skill Extractor",
+                     layout=[[col1],
+                             [col2],
+                             [bottom_row_layout]],
+                     finalize=True)
 
-    match event:
-        case sg.WIN_CLOSED:
+
+def main():
+    """
+    This is the event loop for this application.
+    :return:
+    """
+    main_window = make_main_window()
+    while True:
+        window, event, values = sg.read_all_windows()
+        print(window, event, values)
+
+        if event == sg.WIN_CLOSED and window == main_window:
             break
-        case "exit":
-            break
 
-main_window.close()
+        match event:
+            case "exit":
+                break
+
+    main_window.close()
+
+
+if __name__ == "__main__":
+    main()
