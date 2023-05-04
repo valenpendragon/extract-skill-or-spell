bc53801241f1352aa30aeb7391f562d28fe431f6
Jeffrey Scott <valenpendragon@gmail.com>
2023-05-04 10:22:47 -0400

    Add first pass of NAME_ENDS and EXTRAS to script constants.

---
+++ main.py
@@ -0,0 +1,11 @@
+import PySimpleGUI as sg
+
+# This constant covers the way names of most skill, talents, or spells end and
+# the descriptive text begins.
+NAME_ENDS = ["(1)", "(2)", "(3)",
+             "(T)", "(M)", "(C)", "(S)"]
+EXTRAS = ["Improvised Weapons", "Punch", "Dirty Fighting", "Kick",
+          "Bonus to Damage", "Bonus to Defend", "Two Weapons", "Two Weapons",
+          "Shrewd thrust", "Shrewd blow", "Throw", "Evade", "Prerequisites",
+          "Disarming", "Note", "Show", "Combat", "Believe", "Do Not", "Come",
+          "Fire", "Go That Way", "Kill", "Obey", "Quit", "Stop", "Surrender"]
